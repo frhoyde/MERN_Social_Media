65de11fe894c800542c800e1de24dcd8221b8847
SazidAF <sazidfarhan.a7c@gmail.com>
2022-11-11 09:14:19 +0600

    init App

--- server/index.js
+++ server/index.js
@@ -2,3 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const ConnectionURL = 'mongodb+srv://SazidAF:SazidAF123@cluster0.qseuhca.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(ConnectionURL, { useNewUrlParser: true , useUnifiedTopology: true})
+        .then(() => app.listen(PORT, () => console.log(`Server is running on port: localhost:${PORT}`)))
+        .catch((error) => console.log(error.message));
