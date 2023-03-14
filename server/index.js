6e12b7aa1a9947b9881767a535690dbc43e36e06
SazidAF <sazidfarhan.a7c@gmail.com>
2023-03-15 01:15:01 +0600

    Added Dotenv

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -15,10 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNETCTION_URL = 'mongodb+srv://sazidaf:sazidaf123@cluster0.vj3tx7y.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://sazidaf:sazidaf123@cluster0.vj3tx7y.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONNETCTION_URL, { useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true , useUnifiedTopology: true})
         .then(() => app.listen(PORT, () => console.log(`Server is running on port: localhost:${PORT}`)))
         .catch((error) => console.log(error.message));
 
